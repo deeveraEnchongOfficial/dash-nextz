61d4ab26b3e1716877005bf63b7763a311b97f78
deeveraEnchongOfficial <deveraflorencio18@gmail.com>
2024-02-21 16:09:59 +0800

    setting ui for login and register

---
+++ src/models/User.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isAdmid: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+//If the User collection does not exist create a new one.
+export default mongoose.models.User || mongoose.model("User", userSchema);
