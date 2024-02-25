9127cb2cd68409d91f04bef66a3f02eecbc3c121
deeveraEnchongOfficial <deveraflorencio18@gmail.com>
2024-02-25 08:39:49 +0800

    -wip prisma integration

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,29 +1,28 @@
-import User from "@/models/User";
-import connect from "@/utils/db";
-import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client'
+import bcrypt from "bcryptjs";
+import { connect } from "@/utils/db";
 
 export const POST = async (request: any) => {
-  const { name, email, password } = await request.json();
-
-  await connect();
-
-  const hashedPassword = await bcrypt.hash(password, 5);
-
-  const newUser = new User({
-    name,
-    email,
-    password: hashedPassword,
-  });
-
+  const prisma = new PrismaClient()
   try {
-    await newUser.save();
-    return new NextResponse("User has been created", {
-      status: 201,
-    });
+    const { name, email, password } = await request.json();
+    if (!name || !email || !password) return NextResponse.json({ message: "Please fill in all fields" }, { status: 422 });
+    await connect();
+    const hashedPassword = await bcrypt.hash(password, 5);
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+
+    return NextResponse.json({ message: "User has been created" }, { status: 201 });
   } catch (err: any) {
-    return new NextResponse(err.message, {
-      status: 500,
-    });
+    console.log(err);
+    return NextResponse.json({ message: err.message }, { status: 500 });
+  } finally {
+    await prisma.$disconnect()
   }
 };
